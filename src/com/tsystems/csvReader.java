1f1e7bdc883bda525d3899540fb9f07b0917c220
SZkristof <szabokristof0@gmail.com>
2017-08-15 22:17:08 +0200

    Cleaning up the list comprehension creator

--- src/com/tsystems/csvReader.java
+++ src/com/tsystems/csvReader.java
@@ -1,17 +1,69 @@
 package com.tsystems;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 
 public class csvReader {
-    public static void main(String[] args) {
-        String testFile = "/home/matraiv/quiz_game-t-systems-/src/test.csv";
+
+    private Scanner scanPath;
+
+    public String getPath() {
+        String path = "";
+        try {
+            scanPath = new Scanner(new File("/home/kristof/IdeaProjects/quiz_game-t-systems-/src/csv_path.txt"));
+        } catch (Exception e) {
+            System.out.println("csv_path.txt was not found...");
+        }
+
+        while (scanPath.hasNextLine()) {
+            path += scanPath.nextLine();
+            System.out.println(path);
+
+
+        }
+        return path;
+    }
+
+    private Scanner scannedFile;
+
+    public void openFile() {
+        try {
+            scannedFile = new Scanner(new File(getPath()));
+        } catch (Exception e) {
+            System.out.println("could not find file..");
+        }
+    }
+
+    public void readFile(){
+        List<List<String>> lines = new ArrayList<>();
+        while (scannedFile.hasNextLine()){
+            String line= scannedFile.nextLine();
+            String[] values = line.split(",");
+            // this adds the currently parsed line to the 2-dimensional string array
+            lines.add(Arrays.asList(values));
+        }
+        String a = lines.get(1).get(0);
+        System.out.println(lines);
+    }
+
+    public void closeFile(){
+        scannedFile.close();
+        scanPath.close();
+    }
+}
+
+
+
+
+
+
+
+/*
+         public static void main(String[] args) {
+        String testFile = "/home/kristof/IdeaProjects/quiz_game-t-systems-/src/test.csv";
         File fileName = new File(testFile);
+
 
         //two dimensional arrayList
         List<List<String>> lines = new ArrayList<>();
@@ -38,14 +90,9 @@ public class csvReader {
 
                 }
 
-            } */
+            }
             inputStream.close();
         }catch (FileNotFoundException e) {
             System.err.println("csv file not found");
         }
-
-
-        System.out.println(lines);
-    }
-
-}
+    */
